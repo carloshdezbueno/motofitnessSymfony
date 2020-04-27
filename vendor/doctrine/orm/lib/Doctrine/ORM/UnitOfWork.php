3858feff6a3ff9e8f63bd898a42781287596765e
Carlos <c.hdezbueno@gmail.com>
2020-04-27 19:01:25 +0200

    He cambiado el continue 2, si no va me dices

--- vendor/doctrine/orm/lib/Doctrine/ORM/UnitOfWork.php
+++ vendor/doctrine/orm/lib/Doctrine/ORM/UnitOfWork.php
@@ -2545,7 +2545,7 @@ class UnitOfWork implements PropertyChangedListener
                         $class->reflFields[$field]->setValue($entity, null);
                         $this->originalEntityData[$oid][$field] = null;
 
-                        continue;
+                        continue 2;
                     }
 
                     if ( ! isset($hints['fetchMode'][$class->name][$field])) {
