1b73694f534ecb7565f69286255ef1a4a577ec46
Miguel <macolmenerori.inf@upsa.es>
2020-05-01 17:14:08 +0200

    Página principal de administración maquetada
    Solo interfaz

--- src/MOTO/PrincipalBundle/Controller/AdministracionController.php
+++ src/MOTO/PrincipalBundle/Controller/AdministracionController.php
@@ -8,9 +8,26 @@ class AdministracionController extends Controller
 {
     public function principalAdministracionAction()
     {
-        $nota = "-";
-        // Comprobar que el usuario logueado puede entrar aquí
+        session_start();
         
+        // Si es un empleado
+        $_SESSION['resLogin'] = "empleado";
+        if($_SESSION['resLogin'] == "empleado"){
+            // Comporbar si gestiona dietas
+            // if gestiona dietas == true
+            $dietas = "true";
+            
+            // Comprobar si gestiona tablas
+            $tablas = "true";
+            
+            // Comporbar si gestiona empleados
+            $empleados = "true";
+            
+            return $this->render('MOTOPrincipalBundle:Administracion:principalAdministracion.html.twig', array("login"=>"true", "dietas"=>$dietas, "tablas"=>$tablas, "empleados"=>$empleados));
+        }
+        
+        // Si no es un empleado
+        $nota = "No tienes permisos para acceder aquí";
         return $this->render('MOTOPrincipalBundle:Administracion:principalAdministracion.html.twig', array("nota"=>$nota));
     }
 
