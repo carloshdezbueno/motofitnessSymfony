02c5ea7b8fd014db34b15ea450d4d26220c59947
Carlos <c.hdezbueno@gmail.com>
2020-05-05 10:23:37 +0200

    generado el typoe

---
+++ src/MOTO/PrincipalBundle/Form/EmpleadoType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace MOTO\PrincipalBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class EmpleadoType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('especialidad')
+            ->add('nombre')
+            ->add('dniempleado')
+            ->add('telefono')
+            ->add('email')
+            ->add('direccion')
+            ->add('clave')
+            ->add('privilegios')
+            ->add('dni')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'MOTO\PrincipalBundle\Entity\Empleado'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'moto_principalbundle_empleado';
+    }
+}
