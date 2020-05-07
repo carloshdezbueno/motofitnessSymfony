c15a0ceaec3953de026e0a43c62d5f3e128015e0
Carlos <c.hdezbueno@gmail.com>
2020-05-07 09:55:41 +0200

    Modificado lo de alberto, solo muestra los clientes del empleado

--- src/MOTO/PrincipalBundle/Controller/AdministracionController.php
+++ src/MOTO/PrincipalBundle/Controller/AdministracionController.php
@@ -3,13 +3,10 @@
 namespace MOTO\PrincipalBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
 use MOTO\PrincipalBundle\Form\EmpleadoType;
 use MOTO\PrincipalBundle\Entity\Empleado;
-
 use MOTO\PrincipalBundle\Form\EjercicioType;
 use MOTO\PrincipalBundle\Entity\Ejercicio;
-
 use MOTO\PrincipalBundle\Form\DietaType;
 use MOTO\PrincipalBundle\Entity\Dieta;
 
@@ -90,35 +87,35 @@ class AdministracionController extends Controller {
             }
         }
 
-        return $this->render('MOTOPrincipalBundle:Administracion:asignarDieta.html.twig', array("administrador" => "true",'form' => $formClientes->createView(), 'error' => '-'));
+        return $this->render('MOTOPrincipalBundle:Administracion:asignarDieta.html.twig', array("administrador" => "true", 'form' => $formClientes->createView(), 'error' => '-'));
     }
 
     public function crearDietaAction() {
-        
+
         $error = "-";
         $request = $this->getRequest();
-        
+
         $dieta = new Dieta();
         $form = $this->createForm(new DietaType(), $dieta);
-        
-        if($request->getMethod() == 'POST'){
+
+        if ($request->getMethod() == 'POST') {
             $form->bind($request);
-            
-            if($form->isValid()){
+
+            if ($form->isValid()) {
                 $em = $this->getDoctrine()->getEntityManager();
-                
-                try{
+
+                try {
                     $em->persist($dieta);
                     $em->flush();
                 } catch (Exception $ex) {
                     $error = "Error al crear dieta";
-                    return $this->render('MOTOPrincipalBundle:Administracion:crearDieta.html.twig', array('form' => $form->createView(), 'error'=>$error));
+                    return $this->render('MOTOPrincipalBundle:Administracion:crearDieta.html.twig', array('form' => $form->createView(), 'error' => $error));
                 }
                 return $this->redirect($this->generateUrl('moto_principal_homepage'));
             }
         }
-        
-        return $this->render('MOTOPrincipalBundle:Administracion:crearDieta.html.twig', array('form' => $form->createView(), 'error'=>$error));
+
+        return $this->render('MOTOPrincipalBundle:Administracion:crearDieta.html.twig', array('form' => $form->createView(), 'error' => $error));
     }
 
     public function verDietaAction() {
@@ -140,23 +137,40 @@ class AdministracionController extends Controller {
 
                 $dietaSelec = $form->get("dieta")->getData();
 
-                return $this->render('MOTOPrincipalBundle:Administracion:verDieta.html.twig', array("administrador" => "true",'dietaMostrar' => $dietaSelec));
+                return $this->render('MOTOPrincipalBundle:Administracion:verDieta.html.twig', array("administrador" => "true", 'dietaMostrar' => $dietaSelec));
             }
         }
 
-        return $this->render('MOTOPrincipalBundle:Administracion:verDieta.html.twig', array("administrador" => "true",'form' => $form->createView()));
+        return $this->render('MOTOPrincipalBundle:Administracion:verDieta.html.twig', array("administrador" => "true", 'form' => $form->createView()));
     }
 
     public function asignarTablaClienteAction() {
+
+        $request = $this->getRequest();
+        $session = $request->getSession();
+
+        $em = $this->getDoctrine()->getEntityManager();
+        $consultaEmpleado = "select e from MOTOPrincipalBundle:Empleado e where e.numeroempleado=" . $session->get('dni');
+        $queryEmpleado = $em->createQuery($consultaEmpleado);
+        $empleados = $queryEmpleado->getResult();
+
+        $clientesEmpleado = array();
+
+
+        foreach ($empleados[0]->getDni() as $cli) {
+            $clientesEmpleado[$cli->getDni()] = $cli;
+        }
+
+
         $formClientes = $this->createFormBuilder()
-                ->add('cliente', 'entity', array(
-                    'class' => 'MOTOPrincipalBundle:Cliente'
+                ->add('cliente', 'choice', array(
+                    'choices' => $clientesEmpleado
                 ))
                 ->add('tablaejercicios', 'entity', array(
                     'class' => 'MOTOPrincipalBundle:Tablaejercicios'
                 ))
                 ->getForm();
-        $request = $this->getRequest();
+
 
         if ($request->getMethod() == 'POST') {
 
@@ -165,22 +179,24 @@ class AdministracionController extends Controller {
 
             if ($formClientes->isValid()) {
 
-                $em = $this->getDoctrine()->getEntityManager();
-
                 $cliSelect = $formClientes->get("cliente")->getData();
                 $tabSelect = $formClientes->get("tablaejercicios")->getData();
 
-                $cliSelect->setCodtabla($tabSelect);
+                $consultaCliente = "select c from MOTOPrincipalBundle:Cliente c where c.dni=" . $cliSelect;
+                $queryCliente = $em->createQuery($consultaCliente);
+                $clienteMod = $queryCliente->getResult();
 
-                $em->persist($cliSelect);
+                $clienteAModificar = $clienteMod[0];
+                $clienteAModificar->setCodtabla($tabSelect);
+
+                $em->persist($clienteAModificar);
                 $em->flush();
 
                 return $this->redirect($this->generateUrl('principal_administracion'));
             }
         }
 
-        return $this->render('MOTOPrincipalBundle:Administracion:asignarTablaCliente.html.twig', array("administrador" => "true",'form' => $formClientes->createView(), 'error' => '-'));
-        
+        return $this->render('MOTOPrincipalBundle:Administracion:asignarTablaCliente.html.twig', array("administrador" => "true", 'form' => $formClientes->createView(), 'error' => '-'));
     }
 
     public function nuevaTablaAction() {
@@ -192,61 +208,61 @@ class AdministracionController extends Controller {
     }
 
     public function nuevoEjercicioAction() {
-        
+
         $error = "-";
         $request = $this->getRequest();
-        
+
         $ejercicio = new Ejercicio();
         $form = $this->createForm(new EjercicioType(), $ejercicio);
-        
-        if($request->getMethod() == 'POST'){
+
+        if ($request->getMethod() == 'POST') {
             $form->bind($request);
-            
-            if($form->isValid()){
+
+            if ($form->isValid()) {
                 $em = $this->getDoctrine()->getEntityManager();
-                
-                try{
+
+                try {
                     $em->persist($ejercicio);
                     $em->flush();
                 } catch (Exception $ex) {
                     $error = "Error al guardar en base de datos";
-                    return $this->render('MOTOPrincipalBundle:Administracion:nuevoEjercicio.html.twig', array('form' => $form->createView(), 'error'=>$error));
+                    return $this->render('MOTOPrincipalBundle:Administracion:nuevoEjercicio.html.twig', array('form' => $form->createView(), 'error' => $error));
                 }
                 return $this->redirect($this->generateUrl('moto_principal_homepage'));
             }
         }
-        
-        return $this->render('MOTOPrincipalBundle:Administracion:nuevoEjercicio.html.twig', array('form' => $form->createView(), 'error'=>$error));
+
+        return $this->render('MOTOPrincipalBundle:Administracion:nuevoEjercicio.html.twig', array('form' => $form->createView(), 'error' => $error));
     }
 
     public function nuevoEmpleadoAction() {
-        
+
         $error = "-";
         $request = $this->getRequest();
-        
+
         $empleado = new Empleado();
         $form = $this->createForm(new EmpleadoType(), $empleado);
-        
-        if($request->getMethod() == 'POST'){
+
+        if ($request->getMethod() == 'POST') {
             $form->bind($request);
-            
-            if($form->isValid()){
+
+            if ($form->isValid()) {
                 $em = $this->getDoctrine()->getEntityManager();
-                
-                try{
+
+                try {
                     $em->persist($empleado);
                     $em->flush();
                 } catch (Exception $ex) {
                     $error = "Ha habido un problema. ¿El DNI introducido ya existe?";
                     // Mostrar error
-                    return $this->render('MOTOPrincipalBundle:Administracion:nuevoEmpleado.html.twig', array('form'=>$form->createView(), 'error'=>$error));
+                    return $this->render('MOTOPrincipalBundle:Administracion:nuevoEmpleado.html.twig', array('form' => $form->createView(), 'error' => $error));
                 }
                 // Página principal
                 return $this->redirect($this->generateUrl('moto_principal_homepage'));
             }
         }
         // Renderizar formulario
-        return $this->render('MOTOPrincipalBundle:Administracion:nuevoEmpleado.html.twig', array('form'=>$form->createView(), 'error'=>$error));
+        return $this->render('MOTOPrincipalBundle:Administracion:nuevoEmpleado.html.twig', array('form' => $form->createView(), 'error' => $error));
     }
 
     public function gestionarEmpleadosAction() {
@@ -260,15 +276,15 @@ class AdministracionController extends Controller {
                     'class' => 'MOTOPrincipalBundle:Cliente'
                 ))
                 ->getForm();
-        
+
         $request = $this->getRequest();
 
         if ($request->getMethod() == 'POST') {
             $formClientes->bind($request);
-            
+
             if ($formClientes->isValid()) {
                 $cliSelect = $formClientes->get("cliente")->getData();
-                
+
                 $clienteObtenido = array(
                     'dni' => $cliSelect->getDni(),
                     'nombre' => $cliSelect->getNombre(),
@@ -283,15 +299,14 @@ class AdministracionController extends Controller {
                     'vencimiento' => $cliSelect->getVencimiento() // NO -> Convertir Date a String
                 );
 
-                return $this->render('MOTOPrincipalBundle:Administracion:buscarCliente.html.twig', array('administrador'=>'true', 'cliente'=>$cliSelect, 'clienteobtenido'=>$clienteObtenido));
+                return $this->render('MOTOPrincipalBundle:Administracion:buscarCliente.html.twig', array('administrador' => 'true', 'cliente' => $cliSelect, 'clienteobtenido' => $clienteObtenido));
             }
-            
+
             // Si el cliente no se encuentra mensaje
-            return $this->render('MOTOPrincipalBundle:Administracion:buscarCliente.html.twig', array('administrador'=>'true', 'error'=>'Cliente no encontrado'));
+            return $this->render('MOTOPrincipalBundle:Administracion:buscarCliente.html.twig', array('administrador' => 'true', 'error' => 'Cliente no encontrado'));
         }
-        
-        return $this->render('MOTOPrincipalBundle:Administracion:buscarCliente.html.twig', array('administrador'=>'true', 'form' => $formClientes->createView(), 'error' => '-'));
-        
+
+        return $this->render('MOTOPrincipalBundle:Administracion:buscarCliente.html.twig', array('administrador' => 'true', 'form' => $formClientes->createView(), 'error' => '-'));
     }
 
 }
